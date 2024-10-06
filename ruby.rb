b8d6a2fb89d66beca9fcc8801c05488bb311b582
Lincoln Digby <lincolndigby0@gmail.com>
2024-10-06 14:33:58 -0500

    Remake substrings

--- ruby.rb
+++ ruby.rb
@@ -1,18 +1,15 @@
-require "pry-byebug"
-
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(string, dict)
-
+    string.downcase!
     hash = {}
-
     dict.each do |word|
-        string = string.downcase
-        count = string.scan(word).length
-        hash[word] = count if count > 0
+        arr = string.scan(word)
+        count = arr.length
+        next if count == 0
+        hash[word] = count
     end
-
-    return hash
+    hash
 end
 
 p substrings("Howdy partner, sit down! How's it going?", dictionary)
